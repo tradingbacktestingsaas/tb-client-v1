7ec9a2942edc3355ce73c9f0933a77c125b22b7e
Asad Tanvir Shami <91515695+asadtanvirshami@users.noreply.github.com>
2025-10-17 04:37:57 +0500

    push

--- src/redux/slices/strategy/slice.ts
+++ src/redux/slices/strategy/slice.ts
@@ -1,29 +1,28 @@
-import { StrategyData } from "@/types/strategy-type/type";
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+// import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-export interface StrategyState {
-  values: StrategyData | null;
-  edit: boolean;
-}
+// export interface StrategyState {
+//   values:  null;
+//   edit: boolean;
+// }
 
-const initialState: StrategyState = {
-  values: null,
-  edit: false,
-};
+// const initialState: StrategyState = {
+//   values: null,
+//   edit: false,
+// };
 
-const strategySlice = createSlice({
-  name: "strategy",
-  initialState,
-  reducers: {
-    setStrategyValues: (state, action: PayloadAction<StrategyData>) => {
-      state.values = action.payload;
-    },
-    setEditStrategy: (state, action: PayloadAction<boolean>) => {
-      state.edit = action.payload;
-    },
-  },
-});
+// const strategySlice = createSlice({
+//   name: "strategy",
+//   initialState,
+//   reducers: {
+//     setStrategyValues: (state, action: PayloadAction<StrategyData>) => {
+//       state.values = action.payload;
+//     },
+//     setEditStrategy: (state, action: PayloadAction<boolean>) => {
+//       state.edit = action.payload;
+//     },
+//   },
+// });
 
-export const { setStrategyValues, setEditStrategy } = strategySlice.actions;
+// export const { setStrategyValues, setEditStrategy } = strategySlice.actions;
 
-export default strategySlice.reducer;
+// export default strategySlice.reducer;
